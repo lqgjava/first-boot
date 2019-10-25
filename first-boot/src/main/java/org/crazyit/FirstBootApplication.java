47a9bed03574bbfe45c15cfcbb881418d7d38afc
lqgjava <835185308@qq.com>
2019-10-25 15:43:32 +0800

    hffh

---
+++ first-boot/src/main/java/org/crazyit/FirstBootApplication.java
@@ -0,0 +1,13 @@
+package org.crazyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootApplication.class, args);
+	}
+
+}
---
+++ first-boot/src/test/java/org/crazyit/FirstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crazyit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
